cebccef6afceb52b6a905faf87ecf60e7d8da464
aleixgab <aleix.gab@gmail.com>
2017-09-16 14:53:17 +0200

    XML

---
+++ XML/Motor2D/p2Defs.h
@@ -0,0 +1,86 @@
+#ifndef __P2DEFS_H__
+#define __P2DEFS_H__
+
+#include <stdio.h>
+
+//  NULL just in case ----------------------
+
+#ifdef NULL
+#undef NULL
+#endif
+#define NULL  0
+
+// Deletes a buffer
+#define RELEASE( x ) \
+    {                        \
+    if( x != NULL )        \
+	    {                      \
+      delete x;            \
+	  x = NULL;              \
+	    }                      \
+    }
+
+// Deletes an array of buffers
+#define RELEASE_ARRAY( x ) \
+    {                              \
+    if( x != NULL )              \
+	    {                            \
+      delete[] x;                \
+	  x = NULL;                    \
+	    }                            \
+                              \
+    }
+
+#define IN_RANGE( value, min, max ) ( ((value) >= (min) && (value) <= (max)) ? 1 : 0 )
+#define MIN( a, b ) ( ((a) < (b)) ? (a) : (b) )
+#define MAX( a, b ) ( ((a) > (b)) ? (a) : (b) )
+#define TO_BOOL( a )  ( (a != 0) ? true : false )
+
+typedef unsigned int uint;
+typedef unsigned char uchar;
+
+template <class VALUE_TYPE> void SWAP(VALUE_TYPE& a, VALUE_TYPE& b)
+{
+	VALUE_TYPE tmp = a;
+	a = b;
+	b = tmp;
+}
+
+// Standard string size
+#define SHORT_STR	32
+#define MID_STR		255
+#define HUGE_STR	8192
+
+// TODO 6: Improve config.xml to store all configuration variables that we have as macros
+// Use a section with the name of each module (see Module::name)
+
+/*#define FULLSCREEN 	App->node.child("FULLSCREEN").child_value()
+#define BORDERLESS 	App->node.child("BORDERLESS").child_value()
+#define RESIZABLE	App->node.child("RESIZABLE").child_value()
+#define FULLSCREEN_WINDOW  App->node.child("FULLSCREEN_WINDOW").child_value()
+#define TITLE  App->node.child("TITLE").child_value()
+#define WIDTH  App->node.child("WIDTH").child_value()
+#define HEIGHT App->node.child("HEIGHT").child_value()
+#define SCALE  App->node.child("SCALE").child_value.()*/
+
+
+/*#define FULLSCREEN false
+#define BORDERLESS 	false
+#define RESIZABLE	true
+#define FULLSCREEN_WINDOW  false
+#define TITLE  "replaceme tilte"
+#define WIDTH  1024
+#define HEIGHT 768
+#define SCALE  1*/
+
+
+
+// Joins a path and file
+inline const char* const PATH(const char* folder, const char* file)
+{
+	static char path[MID_STR];
+	sprintf_s(path, MID_STR, "%s/%s", folder, file);
+	return path;
+}
+
+#endif
